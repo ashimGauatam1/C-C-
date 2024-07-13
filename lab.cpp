7d1ff2d2ad6565de1fdec41e444c6a299c2c268f
ramchandraGautam1 <ramchandra.nesdo@gmail.com>
2024-07-13 21:29:01 +0545

    added

--- lab.cpp
+++ lab.cpp
@@ -1,5 +1,47 @@
 #include<iostream>
 using namespace std;
+
+struct Node{
+	int data;
+	Node* next;	
+	Node* prev;
+};
+
+class List{
+	Node*head;
+	public:
+		List(){
+			head=nullptr;
+		}
+		void addb(int item){
+			Node *newnode=new Node;
+			newnode->data=item;
+			newnode->next=head;
+			head=newnode;
+		}
+		void addend(int item){
+			Node* newnode=new Node;
+			newnode->data=item;
+			newnode->next=nullptr;
+			if(head==nullptr){
+				head=newnode;
+			}
+			else{
+				Node*temp=head;
+				while(temp->next!=nullptr){
+					temp=temp->next;
+				}
+				temp->next=newnode;
+			}
+		}
+		void delb(){
+			if(head==nullptr){
+				return;
+			}
+			Node* temp=head;
+			head=head->next;
+			delete temp;
+		}
 		void delatlast(){
 			if(head==nullptr){
 				return;
